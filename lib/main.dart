9e5a6467c591cfcd4dfac6eb9f3c73d4ddd5e0e4
reika178 <rechiko178@gmail.com>
2020-07-10 07:18:40 +0900

    stack

--- lib/main.dart
+++ lib/main.dart
@@ -9,31 +9,45 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var grid = ["pic0", "pic1", "pic2", "pic3", "pic4", "pic5",];
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(
-              title: Text('GridView'),
+              title: Text('Stack'),
             ),
-            body: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  if (index >= grid.length) {
-                    grid.addAll(["pic0", "pic1", "pic2", "pic3", "pic4", "pic5",]);
-                  }
-                  return _photoItem(grid[index]);
-                }
+            body: Stack(
+  alignment: Alignment.bottomRight,
+  children: <Widget>[
+    SizedBox(
+      width: 400.0,
+      height: 400.0,
+      child: Container(color: Colors.orange,),
+    ),
+    Positioned(
+      left: 20.0,
+      top: 20.0,
+      width: 300.0,
+      height: 300.0,
+      child: Container(color: Colors.blue,),
+    ),
+    Positioned(
+      left: 10.0,
+      top: 10.0,
+      width: 100.0,
+      height: 100.0,
+      child: Container(color: Colors.green,),
+    ),
+    Positioned(
+      left: 120.0,
+      top: 120.0,
+      width: 100.0,
+      height: 100.0,
+      child: Container(color: Colors.orange,),
+    ),
+    Text('Test')
+  ],
+
             )
         )
-    );
-  }
-
-  Widget _photoItem(String image) {
-    var assetsImage = "assets/img/" + image + ".png";
-    return Container(
-      child: Image.asset(assetsImage, fit: BoxFit.cover,),
     );
   }
 }
